b99866b9e36a5020d54ca083070c55fad8fd1fb3
Matt Constable <matthew.constable@gmail.com>
2025-06-03 22:42:04 +0200

    Delete custom_components/digigate directory

--- custom_components/digigate/__init__.py
+++
@@ -1,6 +0,0 @@
-async def async_setup_entry(hass, entry):
-    hass.data.setdefault("digigate", {})
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "cover")
-    )
-    return True
