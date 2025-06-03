25d5052123e55a75fd70011322514550f41d8c35
Matt Constable <matthew.constable@gmail.com>
2025-06-03 22:31:39 +0200

    fix

--- custom_components/digigate/__init__.py
+++ custom_components/digigate/__init__.py
@@ -1,11 +1,6 @@
-DOMAIN = "digigate"
-
 async def async_setup_entry(hass, entry):
-    hass.data.setdefault(DOMAIN, {})
+    hass.data.setdefault("digigate", {})
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "cover")
     )
     return True
-
-async def async_unload_entry(hass, entry):
-    return await hass.config_entries.async_forward_entry_unload(entry, "cover")
