41637e2d74d9ffcc399c418ab6ee084d01ae76e0
Matt Constable <matthew.constable@gmail.com>
2025-06-03 22:23:40 +0200

    adding latch back

--- custom_components/digigate/cover.py
+++ custom_components/digigate/cover.py
@@ -1,15 +1,10 @@
-from homeassistant.components.cover import CoverEntity, CoverEntityFeature
-import aiohttp
-
-async def async_setup_entry(hass, entry, async_add_entities):
-    async_add_entities([DigiGateCover(entry)])
-
 class DigiGateCover(CoverEntity):
     def __init__(self, entry):
         self._name = "DigiGate"
         self.api_code = entry.data["api_code"]
         self.ip = entry.data["ip"]
-        self.duration = entry.data.get("duration", 15)  # Default to 15 seconds
+        self.open_mode = entry.data.get("open_mode", "latch")
+        self.duration = entry.data.get("duration", 3)
 
     @property
     def name(self):
@@ -17,14 +12,17 @@ class DigiGateCover(CoverEntity):
 
     @property
     def is_closed(self):
-        return None  # State unknown → both buttons stay active
+        return None
 
     @property
     def supported_features(self):
         return CoverEntityFeature.OPEN | CoverEntityFeature.CLOSE
 
     async def async_open_cover(self, **kwargs):
-        await self._send_command("latch", duration=str(self.duration))
+        if self.open_mode == "timed":
+            await self._send_command("open", duration=str(self.duration))
+        else:
+            await self._send_command("latch", duration="0")
         self.async_write_ha_state()
 
     async def async_close_cover(self, **kwargs):
@@ -34,7 +32,7 @@ class DigiGateCover(CoverEntity):
     async def _send_command(self, request, duration=None):
         url = f"http://{self.ip}:8080/API"
         payload = {"apiCode": self.api_code, "request": request}
-        if duration:
+        if duration is not None:
             payload["duration"] = duration
         async with aiohttp.ClientSession() as session:
             async with session.post(url, json=payload) as resp:
