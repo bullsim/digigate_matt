3dd94e2eff4f59a9e4ba3852530ff3d1f9c1bfab
Matt Constable <matthew.constable@gmail.com>
2025-06-03 22:27:03 +0200

    adding branching logic for config

--- custom_components/digigate/config_flow.py
+++ custom_components/digigate/config_flow.py
@@ -1,19 +1,38 @@
 from homeassistant import config_entries
 import voluptuous as vol
 
-DEFAULT_DURATION = 3
+DEFAULT_DURATION = 15
 
 class DigiGateConfigFlow(config_entries.ConfigFlow, domain="digigate"):
     async def async_step_user(self, user_input=None):
+        """Step 1: Choose mode"""
         if user_input is not None:
-            return self.async_create_entry(title="DigiGate", data=user_input)
+            self.mode = user_input["open_mode"]
+            return await self.async_step_config()
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required("ip"): str,
-                vol.Required("api_code"): str,
-                vol.Required("open_mode", default="latch"): vol.In(["latch", "timed"]),
-                vol.Optional("duration", default=DEFAULT_DURATION): vol.All(int, vol.Range(min=1))
+                vol.Required("open_mode", default="latch"): vol.In(["latch", "timed"])
             })
+        )
+
+    async def async_step_config(self, user_input=None):
+        """Step 2: Enter config based on mode"""
+        fields = {
+            vol.Required("ip"): str,
+            vol.Required("api_code"): str,
+        }
+
+        if self.mode == "timed":
+            fields[vol.Required("duration", default=DEFAULT_DURATION)] = vol.All(int, vol.Range(min=1))
+
+        if user_input is not None:
+            # Save mode from first step into final data
+            user_input["open_mode"] = self.mode
+            return self.async_create_entry(title="DigiGate", data=user_input)
+
+        return self.async_show_form(
+            step_id="config",
+            data_schema=vol.Schema(fields)
         )
